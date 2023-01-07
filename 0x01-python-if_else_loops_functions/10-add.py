551780759fa60be6a41910e5caca5b865432aea7
Abdul1091 <abdul2kur1@gmail.com>
2023-01-07 12:43:31 -0800

    Pyhon3

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Bamidele Adefolaju
+def add(a, d):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Abdullahi Tukur
+
+def magic_calculator(a, d, c):
+    """Math bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
