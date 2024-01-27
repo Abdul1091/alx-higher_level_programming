1c01db5fa39d6993200c372a47150689ac7a6f30
Abdul1091 <Abdul2kur1gmail.com>
2024-01-27 11:27:51 -0800

    PythonStruct

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for k in i:
+            if k == i[-1]:
+                print('{:d}'.format(k), end='')
+            else:
+                print('{:d}'.format(k), end=' ')
+        print()
